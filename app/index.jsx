93f93acb32b17642f648ee7380d5a28a14b2f78e
Ayhan <ayhan-games@hotmail.com>
2025-02-11 22:44:59 +0300

    project initialized nativewind setted

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-red-500 text-3xl">Edit appss/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
