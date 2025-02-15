a3bbfe52dcce5c26a0d747691174cc817099b905
Ayhan <ayhan-games@hotmail.com>
2025-02-15 22:13:50 +0300

    [id] updated

--- app/detail/[id].jsx
+++ app/detail/[id].jsx
@@ -1,39 +1,47 @@
-import { View, Text, Image, TouchableOpacity, ScrollView, FlatList, Linking } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  Linking,
+} from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import bg from "../../assets/menubackground.jpg";
 import { UserContext } from "../../context/UserContext";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import Entypo from '@expo/vector-icons/Entypo';
+import Entypo from "@expo/vector-icons/Entypo";
 import { useRouter } from "expo-router";
 import axios from "axios";
-import key from "../../key/API"
+import key from "../../key/API";
 
 const Detail = () => {
   const { singleData } = useContext(UserContext);
-  const [detailData,setDetailData] = useState([]);
+  const [detailData, setDetailData] = useState([]);
   const router = useRouter();
 
-  const getSpecificData = async()=>{
+  const getSpecificData = async () => {
     try {
-      const response = await axios.get(`https://api.themoviedb.org/3/movie/${singleData.id}?api_key=${key.APIKEY}&language=en-US`)
-      setDetailData(response.data)
-      console.log("Movie Data:", response.data);
-
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${singleData.id}?api_key=${key.APIKEY}&language=en-US`
+      );
+      setDetailData(response.data);
     } catch (error) {
       try {
-        const response = await axios.get(`https://api.themoviedb.org/3/tv/${singleData.id}?api_key=${key.APIKEY}&language=en-US`)
-      setDetailData(response.data)
-      console.log("TV Show Data:", response.data);
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/tv/${singleData.id}?api_key=${key.APIKEY}&language=en-US`
+        );
+        setDetailData(response.data);
       } catch (error) {
         return null;
       }
-      
     }
-  }
-  useEffect(()=>{
-    getSpecificData()
-  },[])
+  };
+  useEffect(() => {
+    getSpecificData();
+  }, []);
 
   return (
     <ScrollView className="w-full h-full bg-black relative">
@@ -58,7 +66,9 @@ const Detail = () => {
         </Text>
         <Text className="text-gray-400 text-lg mt-2">
           {singleData.release_date} {"  |  "} ⭐ IMDb{" "}
-          {singleData.vote_average.toFixed(1)} {"  |  "} ⏳ {detailData.runtime} min | {"  |  "}{singleData.original_language.toUpperCase()}
+          {singleData.vote_average.toFixed(1)} {"  |  "} ⏳ {detailData.runtime}{" "}
+          min | {"  |  "}
+          {singleData.original_language.toUpperCase()}
         </Text>
 
         <Text className="text-gray-300 mt-4 text-base">
@@ -76,58 +86,60 @@ const Detail = () => {
           <Text className="text-white text-lg font-bold ml-2">Watchlist</Text>
         </TouchableOpacity>
       </View>
-      <View className="w-full h-28 p-5 flex-row gap-16 ">
-      <View className="items-center gap-3">
-      <AntDesign name="pluscircleo" size={30} color="gray" />
-      <Text className="text-white text-xs">Add to favorite</Text>
-      </View>
-      <View className="items-center gap-3">
-      <AntDesign name="like1" size={30} color="gray" />
-      <Text className="text-white text-xs">Like</Text>
-      </View>
-      <View className="items-center gap-3">
-      <Entypo name="share-alternative" size={30} color="gray" />
-      <Text className="text-white text-xs">Share</Text>
-      </View>
+      <View className="w-full h-28 p-5 flex-row gap-10 ">
+        <View className="items-center gap-3">
+          <AntDesign name="pluscircleo" size={30} color="gray" />
+          <Text className="text-white text-xs">Add to favorite</Text>
+        </View>
+        <View className="items-center gap-3">
+          <AntDesign name="like1" size={30} color="gray" />
+          <Text className="text-white text-xs">Like</Text>
+        </View>
+        <View className="items-center gap-3">
+          <Entypo name="share-alternative" size={30} color="gray" />
+          <Text className="text-white text-xs">Share</Text>
+        </View>
       </View>
       <View className="items-center">
-        <FlatList
-        className="pt-3"
-        contentContainerStyle={{gap:4}}
-        data={detailData.genres}
-        horizontal
-        keyExtractor={(item)=>item.id}
-        renderItem={({item})=>(
-          <View className="w-24 h-10 border border-gray-500 flex items-center justify-center rounded-md">
-      <Text className="text-sm text-white font-semibold">{item.name}</Text>
-    </View>
-        )}
-        />
+        <View className="flex-row flex-grow gap-2">
+        {
+          detailData.genres?.map((item)=>(
+            <View key={item.id} className="w-24 h-10 border border-gray-500 flex items-center justify-center rounded-md">
+              <Text className="text-sm text-white font-semibold">
+                {item.name}
+              </Text>
+            </View>
+          ))
+        }
+        </View>
       </View>
       {detailData.tagline ? (
-          <Text className="text-gray-300 italic text-center pt-10">"{detailData.tagline}"</Text>
-        ) : null}
-        <View className="pt-10 px-6">
-        <Text className="text-gray-400 text-lg font-semibold">Production Companies</Text>
-          <FlatList
-          data={detailData.production_companies}
-          keyExtractor={(item)=>item.id}
-          renderItem={({item})=>(
+        <Text className="text-gray-300 italic px-5 pt-10">
+          "{detailData.tagline}"
+        </Text>
+      ) : null}
+      <View className="pt-10 px-6">
+        <Text className="text-gray-400 text-lg font-semibold">
+          Production Companies
+        </Text>
+        {
+          detailData.production_companies?.map((item)=>(
             <Text key={item.id} className="text-gray-300 text-sm">
               • {item.name}
             </Text>
-          )}
-          />
-
-        </View>
-        <View className="w-full items-center">
+          ))
+        }
+      </View>
+      <View className="w-full items-center">
         <TouchableOpacity
           className="bg-yellow-500 w-[90%] p-3 mt-5 rounded-lg items-center "
-          onPress={() => Linking.openURL(`https://www.imdb.com/title/${detailData.imdb_id}`)}
+          onPress={() =>
+            Linking.openURL(`https://www.imdb.com/title/${detailData.imdb_id}`)
+          }
         >
           <Text className="text-black font-bold">📽 View on IMDb</Text>
         </TouchableOpacity>
-        </View>
+      </View>
     </ScrollView>
   );
 };
